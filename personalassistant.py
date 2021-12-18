f862c5b34f0b050cbc13eeaf291ab5c13def2e70
AnnMol-2002 <83901287+AnnMol-2002@users.noreply.github.com>
2021-12-18 20:06:53 +0530

    Update personalassistant.py

--- personalassistant.py
+++ personalassistant.py
@@ -2,12 +2,15 @@ from urllib import request
 import pyttsx3
 import datetime
 import pywhatkit
-
+import wikipedia
 import requests
+import pyjokes 
 from bs4 import BeautifulSoup
  
 import speech_recognition as s
+
 w=pyttsx3.init()
+
 def speak(a):
     w.say(a)
     w.runAndWait()
@@ -20,34 +23,84 @@ def recognise():
         try:
             command=l.recognize_google(voice)
             return command
+
         except:
             a="please say again"
             speak(a)
-command = recognise()
-print(command)
-command=command.lower()
+            return a
+def myname():
+    speak("hey, echo here....what can i call you")
+    name=input("enter your name")
+    speak("hii " + name +" nice to meet you")
+    print("hii " + name +" nice to meet you")
 
-if 'chrome' in command:
-    a="Opening Chrome...."
-    speak(a)
-    pywhatkit.search(command)
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Good Morning, what can i do for you")
+        print("Good Morning, what can i do for you")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon, what can i do for you ")
+        print("Good Afternoon, what can i do for you")
+    else:
+        speak("Good Evening, what can i do for you")
+        print("Good Evening, what can i do for you")
 
-if 'time' in command:
-    time=datetime.datetime.now().strftime("%I:%M:%p")
-    print(time)
-    speak(time)
-h=['play','video','show']
-if any(x in command for x in h ):
-    b='Opening youtube....'
-    speak(b)
-    pywhatkit.playonyt(command)
+myname()
+wishMe()
+
+key_wiki=['what','who', 'about']
+key_utube=['play','show','watch']
+key_temp=['weather','temperature','climate']
+
+while(1):
+    command = recognise()
+    print(command)
+    command=command.lower()
+
+    if 'search' in command:
+        a="Opening Chrome...."
+        print(a)
+        speak(a)
+        text=command.replace('search for', ' ')
+        pywhatkit.search(text)
+
+    elif 'time' in command:
+        time=datetime.datetime.now().strftime("%I:%M:%p")
+        print(time)
+        speak(time)
+
+    elif any(x in command for x in key_wiki):
+        speak("wait a second ")
+        result=wikipedia.summary(command,3)
+        print(result)
+        speak(result)
+
+    elif any(x in command for x in key_utube ):
+        b='opening youtube....'
+        print(b)
+        speak(b)
+        pywhatkit.playonyt(command)
+        
+    elif any(x in command for x in key_temp):
+        t="temperature in thrissur"
+        url=f"https://www.google.com/search?q={t}"
+        r=requests.get(url)
+        data=BeautifulSoup(r.text,"html.parser")
+        temp=data.find("div",class_="BNeawe").text
+        speak(f"current{t} is {temp}")       
+        print(f"current{t} is {temp}")
+
+    elif 'joke' in command:
+        j=pyjokes.get_joke()
+        speak(j)
+        print(j)
     
-k=['weather','temperature','climate']
-if any(x in command for x in k):
-    s="temperature in thrissur"
-    url=f"https://www.google.com/search?q={s}"
-    r=requests.get(url)
-    data=BeautifulSoup(r.text,"html.parser")
-    temp=data.find("div",class_="BNeawe").text
-    speak(f"current{s} is {temp}")       
-    print(f"current{s} is {temp}")
+    elif 'bye' in command:
+        speak("okay bye ")
+        hour=datetime.datetime.now().hour
+        if hour>21:
+            speak("good night")
+        else:
+            speak("see you again")
+        break
